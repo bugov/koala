58d7ec5f327214f908691a0a125df80d7abe9ba2
Georgy Bazhukov <georgy.bazhukov@gmail.com>
2013-06-29 02:50:40 +0600

    Rose config -> JSON config

--- lib/Koala/Model/Config.pm
+++ lib/Koala/Model/Config.pm
@@ -1,14 +1,21 @@
 package Koala::Model::Config;
 use base 'Rose::DB';
+use FindBin;
+use JSON::XS;
+use Data::Dumper;
+
+my $config;
+{
+  open(my $fh, "$FindBin::Bin/../conf/database.json")
+    or die "Can't find $FindBin::Bin/../conf/database.json";
+  local $/;
+  $config = <$fh>;
+  close $fh;
+}
+$config = decode_json $config;
 
 __PACKAGE__->use_private_registry;
 __PACKAGE__->default_connect_options( mysql_enable_utf8 => 1 );
-__PACKAGE__->register_db (
-  driver   => 'MySQL',
-  database => 'koala',
-  host     => 'localhost',
-  username => 'koala',
-  password => 'koala'
-);
+__PACKAGE__->register_db ($config->{database});
 
 1;
