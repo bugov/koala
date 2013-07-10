730634d142366838197e20174892f2b3023a3ec5
Бажуков Георгий <bugov@webmotor.ru>
2013-07-10 15:29:32 +0600

    add Config module

---
+++ lib/Koala/Entity/Config.pm
@@ -0,0 +1,42 @@
+# Class: Koala::Entity::Config
+#   Config singleton
+package Koala::Entity::Config;
+use Pony::Object qw/:try -singleton/;
+use FindBin;
+use JSON::XS;
+  
+  protected config => {};
+  
+  # Method: init
+  #   Constructor. Get all configs.
+  sub init : Public
+    {
+      my $this = shift;
+      local $/;
+      
+      for my $file (<$FindBin::Bin/../conf/*.json>) {
+        my $conf = try {
+          open(my $fh, "$FindBin::Bin/../conf/database.json") or die "Can't find $file";
+          my $config = <$fh>;
+          close $fh;
+          return decode_json $config;
+        } catch {
+          return {};
+        };
+        
+        $this->config = { %{$this->config}, $conf };
+      }
+    }
+  
+  # Method: get_config
+  #   Getter for config.
+  #
+  # Returns:
+  #   HashRef
+  sub get_config : Public
+    {
+      my $this = shift;
+      return $this->config;
+    }
+  
+1;
