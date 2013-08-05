51cf911bedfb50e6d40a727131a25f9364aacac0
Georgy Bazhukov <gosha@bugov.net>
2013-08-05 08:19:17 +0600

    use Koala::Controller::Admin::Base in Koala::Controller::Admin::User

--- lib/Koala/Controller/Admin/User.pm
+++ lib/Koala/Controller/Admin/User.pm
@@ -1,35 +1,17 @@
 package Koala::Controller::Admin::User;
-use Mojo::Base 'Mojolicious::Controller';
+use Mojo::Base 'Koala::Controller::Admin::Base';
 use Koala::Model::User;
 
-my $list_size = 20;
+has 'model_name' => 'user';
 
-sub list {
-  my $self = shift;
-  my $page = int $self->param('page');
-  my $user_list = Koala::Model::User::Manager->get_users(
-    sort_by => 'id', limit => $list_size,
-    offset => $list_size * ($page-1)
-  );
-  my $user_count = Koala::Model::User::Manager->get_users_count();
-  $self->render('user/admin/list', user_list => $user_list, user_count => $user_count, limit => $list_size);
-}
-
-sub show {
-  my $self = shift;
-  my $user = Koala::Model::User->new(id => int $self->param('id'))->load();
-  $self->render('user/admin/show', user => $user);
-}
-
-sub edit {
-  my $self = shift;
-  my $user = Koala::Model::User->new(id => int $self->param('id'))->load();
+# Method: _dehydrate
+#   Redefine if you wanna custom work with input.
+sub _dehydrate {
+  my ($self, $user) = @_;
+  my ($model) = $self->_get_model();
   $user->$_($self->param($_)) for qw/username email info role/;
   $user->create_at( $self->dt($self->param('create_at')) );
   $user->new_password($self->param('password')) if $self->param('password');
-  $user->save();
-  $self->flash({message => 'Profile edited', type => 'success'})
-    ->redirect_to('admin_user_show');
 }
 
 1;
